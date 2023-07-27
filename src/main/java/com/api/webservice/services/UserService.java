0d099ff8434f454bf155ed6f94c1a64423d17236
UitaloAndrade <uitalo.a.reis@gmail.com>
2023-07-27 13:21:30 -0300

    Service layer, component registration

---
+++ src/main/java/com/api/webservice/services/UserService.java
@@ -0,0 +1,27 @@
+package com.api.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.webservice.entities.User;
+import com.api.webservice.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findall(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+
+}
