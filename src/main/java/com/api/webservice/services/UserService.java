7164488b00c0e896a70529e680b427ef85aa6851
UitaloAndrade <uitalo.a.reis@gmail.com>
2023-07-29 15:39:16 -0300

    Exception handling - update

--- src/main/java/com/api/webservice/services/UserService.java
+++ src/main/java/com/api/webservice/services/UserService.java
@@ -13,6 +13,8 @@ import com.api.webservice.repositories.UserRepository;
 import com.api.webservice.services.exceptions.DatabaseException;
 import com.api.webservice.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User user) {
+		try {
 		User entity = userRepository.getReferenceById(id);
 		updateData(entity, user);
 		return userRepository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
